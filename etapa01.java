ab79380b755d53649ee990fe14d8b61921f9034e
KalondraAv <avaromavkalondra@gmail.com>
2026-03-04 14:08:51 -0400

    Add files via upload
    Ler nome do cliente. Ler descrição do problema. Ler valor da hora de serviço. Ler quantidade de horas trabalhadas. Calcular o valor total. Exibir um resumo organizado.

---
+++ etapa01.java
@@ -0,0 +1,40 @@
+package projProg;
+import java.util.Scanner;
+
+public class etapa01 {
+
+	public static void main(String[] args) {
+		/**
+		Ler nome do cliente
+		Ler descrição do problema
+		Ler valor da hora de serviço
+		Ler quantidade de horas trabalhadas
+		Calcular o valor total
+		Exibir um resumo organizado
+		**/
+		Scanner teclado = new Scanner(System.in);
+		
+		String nomeC;
+		String nomeT;
+		String prob;
+		float valorHora;
+		double valorTotal;
+		int qtdeHora;
+		
+		System.out.print("-- REGISTRO DE MANUTENÇÃO TÉCNICA --\nInforme seu nome: ");
+		nomeC = teclado.nextLine();
+		System.out.print("Olá, "+nomeC+"! Quem lhe atendeu? ");
+		nomeT = teclado.nextLine();
+		System.out.print("Problema identificado: ");
+		prob =teclado.nextLine();
+		System.out.print("Certo. Digite o valor da hora de serviço: R$ ");
+		valorHora = teclado.nextFloat();
+		System.out.print("Informe a quantidade de horas trabalhadas: ");
+		qtdeHora = teclado.nextInt();
+		
+		System.out.print("\n-- RESUMO DO SERVIÇO --\n"+nomeC+", você foi atendido(a) pelo(a) "+nomeT+", que resolveu o seguinte problema: "+prob+".\nA hora de serviço de "+nomeT+" custa R$ "+valorHora+".\n");
+		valorTotal = valorHora*qtdeHora;
+		System.out.print("Como ele(a) levou "+qtdeHora+" hora(s) para resolver a solicitação, o valor total do serviço é de R$ "+valorTotal+".");
+		}
+
+}
